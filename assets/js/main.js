d071ded5a185c9b812e403afecd64ade9fa82ed7
Zion Obia <obiazion55@gmail.com>
2024-12-25 14:06:57 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnicon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) =>{
+menuBtn.addEventListener("click", (e) => {
   navLinks.classList.toggle("open");
   
   const isOpen = navLinks.classList.contains("open");
   menuBtnicon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-navLinks.addEventListener("click", (e) =>{
+navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnicon.setAttribute("class", "ri-menu-line");
 });
